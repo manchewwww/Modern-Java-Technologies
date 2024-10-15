35a059d16a95822e15ac92465d3ca48122e1ef2a
manchewwww <116086924+manchewwww@users.noreply.github.com>
2024-10-15 13:42:31 +0300

    Add TextJustifier

---
+++ Lab01/src/TextJustifier.java
@@ -0,0 +1,10 @@
+public class TextJustifier {
+    public static String[] justifyText(String[] words, int maxWidth) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
