5caf4ecf97d7c8b885a7fd49f5960b5e9e7ba4eb
manchewwww <georgimanchev2003@gmail.com>
2024-12-16 13:37:25 +0200

    Create SentimentAnalysisException class implementation

--- Lab09-SentimentAnalyzer/src/bg/sofia/uni/fmi/mjt/sentimentnalyzer/exceptions/SentimentAnalysisException.java
+++ Lab09-SentimentAnalyzer/src/bg/sofia/uni/fmi/mjt/sentimentnalyzer/exceptions/SentimentAnalysisException.java
@@ -1,5 +1,13 @@
 package bg.sofia.uni.fmi.mjt.sentimentnalyzer.exceptions;
 
-public class SentimentAnalysisException     {
+public class SentimentAnalysisException extends RuntimeException {
+
+    public SentimentAnalysisException(String message) {
+        super(message);
+    }
+
+    public SentimentAnalysisException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
 }
