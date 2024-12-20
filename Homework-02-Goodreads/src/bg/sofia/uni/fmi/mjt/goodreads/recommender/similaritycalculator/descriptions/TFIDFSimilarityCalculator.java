b4a18b9ca7312874aef734e4e265bb76d697ca7d
manchewwww <georgimanchev2003@gmail.com>
2024-12-20 15:52:00 +0200

    Fix formula in computeIDF

--- Homework-02-Goodreads/src/bg/sofia/uni/fmi/mjt/goodreads/recommender/similaritycalculator/descriptions/TFIDFSimilarityCalculator.java
+++ Homework-02-Goodreads/src/bg/sofia/uni/fmi/mjt/goodreads/recommender/similaritycalculator/descriptions/TFIDFSimilarityCalculator.java
@@ -83,7 +83,8 @@ public class TFIDFSimilarityCalculator implements SimilarityCalculator {
         return words.stream()
             .collect(Collectors.toMap(
                 word -> word,
-                word -> Math.log((double) size / (1 + docFrequency.get(word))
+                word -> docFrequency.get(word) == 0 ? Math.log((double) size) :
+                    Math.log((double) size / docFrequency.get(word)
                 )));
     }
 
