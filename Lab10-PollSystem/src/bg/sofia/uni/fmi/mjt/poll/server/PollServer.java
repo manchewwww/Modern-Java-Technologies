783919816ccc278c5b47ec1f6f15065978177071
manchewwww <georgimanchev2003@gmail.com>
2025-01-10 12:39:22 +0200

    Add server implementation

--- Lab10-PollSystem/src/bg/sofia/uni/fmi/mjt/poll/server/PollServer.java
+++ Lab10-PollSystem/src/bg/sofia/uni/fmi/mjt/poll/server/PollServer.java
@@ -1,5 +1,126 @@
 package bg.sofia.uni.fmi.mjt.poll.server;
 
+import bg.sofia.uni.fmi.mjt.poll.commands.CommandExecutor;
+import bg.sofia.uni.fmi.mjt.poll.server.repository.PollRepository;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+
 public class PollServer {
+
+    private static final int BUFFER_SIZE = 1024;
+    private static final String HOST = "localhost";
+
+    private final CommandExecutor commandExecutor;
+
+    private final int port;
+    private boolean isServerWorking;
+
+    private ByteBuffer buffer;
+    private Selector selector;
+
+    public PollServer(int port, PollRepository pollRepository) {
+        this.port = port;
+        this.commandExecutor = new CommandExecutor(pollRepository);
+    }
+
+    public void start() {
+        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
+            selector = Selector.open();
+            configureServerSocketChannel(serverSocketChannel, selector);
+            this.buffer = ByteBuffer.allocate(BUFFER_SIZE);
+            isServerWorking = true;
+            while (isServerWorking) {
+                try {
+                    int readyChannels = selector.select();
+                    if (readyChannels == 0) {
+                        continue;
+                    }
+
+                    processSelectedKeys();
+                } catch (IOException e) {
+                    System.out.println("Error occurred while processing client request: " + e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException("failed to start server", e);
+        }
+    }
+
+    public void stop() {
+        this.isServerWorking = false;
+        if (selector.isOpen()) {
+            selector.wakeup();
+        }
+    }
+
+    private void configureServerSocketChannel(ServerSocketChannel channel, Selector selector) throws IOException {
+        channel.bind(new InetSocketAddress(HOST, this.port));
+        channel.configureBlocking(false);
+        channel.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+    private void processSelectedKeys() throws IOException {
+        Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
+        while (keyIterator.hasNext()) {
+            SelectionKey key = keyIterator.next();
+            if (key.isReadable()) {
+                SocketChannel clientChannel = (SocketChannel) key.channel();
+                String clientInput = getClientInput(clientChannel);
+                System.out.println(clientInput);
+                if (clientInput == null) {
+                    continue;
+                }
+
+                String output = commandExecutor.execute(clientInput);
+                writeClientOutput(clientChannel, output);
+            } else if (key.isAcceptable()) {
+                accept(selector, key);
+            }
+
+            keyIterator.remove();
+        }
+    }
+
+    private String getClientInput(SocketChannel clientChannel) throws IOException {
+        buffer.clear();
+
+        int readBytes = clientChannel.read(buffer);
+        if (readBytes < 0) {
+            clientChannel.close();
+            return null;
+        }
+
+        buffer.flip();
+
+        byte[] clientInputBytes = new byte[buffer.remaining()];
+        buffer.get(clientInputBytes);
+
+        return new String(clientInputBytes, StandardCharsets.UTF_8);
+    }
+
+    private void writeClientOutput(SocketChannel clientChannel, String output) throws IOException {
+        buffer.clear();
+        buffer.put(output.getBytes());
+        buffer.flip();
+
+        clientChannel.write(buffer);
+    }
+
+    private void accept(Selector selector, SelectionKey key) throws IOException {
+        ServerSocketChannel sockChannel = (ServerSocketChannel) key.channel();
+        SocketChannel accept = sockChannel.accept();
+
+        accept.configureBlocking(false);
+        accept.register(selector, SelectionKey.OP_READ);
+    }
 
 }
