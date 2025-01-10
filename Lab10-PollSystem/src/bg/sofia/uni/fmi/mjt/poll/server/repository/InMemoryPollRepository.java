0153229e6239ef8c8ba2fb851654332d426052e7
manchewwww <georgimanchev2003@gmail.com>
2025-01-10 12:39:44 +0200

    Add implementationof InMemoryPollRepository

--- Lab10-PollSystem/src/bg/sofia/uni/fmi/mjt/poll/server/repository/InMemoryPollRepository.java
+++ Lab10-PollSystem/src/bg/sofia/uni/fmi/mjt/poll/server/repository/InMemoryPollRepository.java
@@ -2,28 +2,40 @@ package bg.sofia.uni.fmi.mjt.poll.server.repository;
 
 import bg.sofia.uni.fmi.mjt.poll.server.model.Poll;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class InMemoryPollRepository implements PollRepository {
 
+    private static final AtomicInteger ID = new AtomicInteger(1);
+    private final Map<Integer, Poll> polls = new ConcurrentHashMap<>();
+
     @Override
     public int addPoll(Poll poll) {
-        return 0;
+        if  (poll == null) {
+            throw new IllegalArgumentException("Poll is null");
+        }
+
+        int id = ID.getAndIncrement();
+        polls.put(id, poll);
+        return id;
     }
 
     @Override
     public Poll getPoll(int pollId) {
-        return null;
+        return polls.get(pollId);
     }
 
     @Override
     public Map<Integer, Poll> getAllPolls() {
-        return Map.of();
+        return Collections.unmodifiableMap(polls);
     }
 
     @Override
     public void clearAllPolls() {
-
+        polls.clear();
     }
 
 }
