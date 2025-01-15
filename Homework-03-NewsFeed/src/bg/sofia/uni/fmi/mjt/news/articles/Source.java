28ad315ad2f7fd2396a0a4b22da718631a222489
manchewwww <georgimanchev2003@gmail.com>
2025-01-15 17:44:42 +0200

    Create class for source

---
+++ Homework-03-NewsFeed/src/bg/sofia/uni/fmi/mjt/news/articles/Source.java
@@ -0,0 +1,21 @@
+package bg.sofia.uni.fmi.mjt.news.articles;
+
+public class Source {
+
+    private String id;
+    private String name;
+
+    public Source(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
