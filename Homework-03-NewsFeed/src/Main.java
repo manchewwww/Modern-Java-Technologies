4c6a0a1c9b2658cf1972eb9b530866a819caf99f
manchewwww <georgimanchev2003@gmail.com>
2025-01-16 22:22:32 +0200

    Some my tests from console

--- Homework-03-NewsFeed/src/Main.java
+++ Homework-03-NewsFeed/src/Main.java
@@ -1,24 +1,36 @@
+import bg.sofia.uni.fmi.mjt.news.builder.Arguments;
 import bg.sofia.uni.fmi.mjt.news.client.NewsClient;
 import bg.sofia.uni.fmi.mjt.news.exceptions.ApiException;
+import bg.sofia.uni.fmi.mjt.news.request.BuildRequest;
 import bg.sofia.uni.fmi.mjt.news.response.OKResponse;
+
+import java.net.http.HttpClient;
 
 public class Main {
 
-    static String apiKEy = "02c74a646ebb4f2f91f754bdd664ba40";
+    static String apiKEy = "MY_API_KEY";
 
     public static void main(String[] args) {
         try {
-            NewsClient newsClient = new NewsClient(apiKEy);
-            //OK WITH ONLY KEYWORDS
-//        OKResponse okResponse = newsClient.getResponse("trump", null, null, 0, 0);
-//        System.out.println(okResponse.getStatus());
-//        System.out.println(okResponse.getTotalResults());
-//        System.out.println(okResponse.getArticles());
+            NewsClient newsClient = new NewsClient(HttpClient.newHttpClient(), apiKEy);
 
-            OKResponse okResponse = newsClient.getResponse("trump", null, "US", 2, 2);
-            System.out.println(okResponse.getStatus());
-            System.out.println(okResponse.getTotalResults());
-            System.out.println(okResponse.getArticles());
+            BuildRequest request = new BuildRequest("dasdas");
+
+            //OK WITH ONLY KEYWORDS
+            Arguments arguments1 = Arguments.builder("trump")
+                .build();
+            OKResponse okResponse1 = newsClient.getResponse(arguments1);
+            System.out.println(okResponse1.status());
+            System.out.println(okResponse1.totalResults());
+            System.out.println(okResponse1.articles().size());
+
+            Arguments arguments2 = Arguments.builder("trump")
+                .setCountry("US")
+                .build();
+            OKResponse okResponse2 = newsClient.getResponse(arguments2);
+            System.out.println(okResponse2.status());
+            System.out.println(okResponse2.totalResults());
+            System.out.println(okResponse2.articles().size());
 
             //ERROR BECAUSE KEYWORD IS NULL
 //            OKResponse okResponse = newsClient.getResponse(null, null, null, 0, 0);
