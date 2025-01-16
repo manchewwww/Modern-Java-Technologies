b85f61b08f565e3911cf2c6f36940b08a7908c50
manchewwww <georgimanchev2003@gmail.com>
2025-01-16 22:17:08 +0200

    Convert class to a record

--- Homework-03-NewsFeed/src/bg/sofia/uni/fmi/mjt/news/response/ErrorResponse.java
+++ Homework-03-NewsFeed/src/bg/sofia/uni/fmi/mjt/news/response/ErrorResponse.java
@@ -1,26 +1,5 @@
 package bg.sofia.uni.fmi.mjt.news.response;
 
-public class ErrorResponse {
+public record ErrorResponse(String error, String code, String message) {
 
-    private final String error;
-    private final String code;
-    private final String message;
-
-    public  ErrorResponse(String error, String code, String message) {
-        this.error = error;
-        this.code = code;
-        this.message = message;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
--- Homework-03-NewsFeed/src/bg/sofia/uni/fmi/mjt/news/response/OKResponse.java
+++ Homework-03-NewsFeed/src/bg/sofia/uni/fmi/mjt/news/response/OKResponse.java
@@ -4,27 +4,6 @@ import bg.sofia.uni.fmi.mjt.news.articles.Article;
 
 import java.util.List;
 
-public class OKResponse {
-    private String status;
-    private int totalResults;
-    private List<Article> articles;
-
-    public OKResponse(String status, int totalResults, List<Article> articles) {
-        this.status = status;
-        this.totalResults = totalResults;
-        this.articles = articles;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public int getTotalResults() {
-        return totalResults;
-    }
-
-    public List<Article> getArticles() {
-        return articles;
-    }
+public record OKResponse(String status, int totalResults, List<Article> articles) {
 
 }
