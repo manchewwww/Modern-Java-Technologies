a6d8092379f9c8152c0e7ca75badaf33adf8ac30
manchewwww <georgimanchev2003@gmail.com>
2025-01-22 17:33:56 +0200

    Add User with username, hashedPassword, wallet and methods for wallet management

---
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/wallet/User.java
@@ -0,0 +1,56 @@
+package bg.sofia.uni.fmi.mjt.crypto.wallet;
+
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.InsufficientFundsException;
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.InvalidAmountOfDepositException;
+
+import java.util.Map;
+
+public class User {
+
+    private final String username;
+    private final String hashedPassword;
+    private final Wallet wallet;
+
+    public User(String username, String hashedPassword) {
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        this.wallet = new Wallet();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public String depositMoney(double amount) throws InvalidAmountOfDepositException {
+        wallet.depositMoney(amount);
+        return "The deposit is successful!";
+    }
+
+    public String buyCrypto(String assetId, double price,  double amount)
+        throws InvalidAmountOfDepositException, InsufficientFundsException {
+        wallet.buyCrypto(assetId, price , amount);
+        return "The purchase is successful!";
+    }
+
+    public String sellCrypto(String assetId, double currentPrice) {
+        wallet.sell(assetId, currentPrice);
+        return "The sale is successful!";
+    }
+
+    public String getWalletSummary() {
+        return String.format("Wallet summary: " + System.lineSeparator() + "%s", wallet.getWalletSummary());
+    }
+
+    public String getWalletOverallSummary(Map<String, Double> currentPrices) {
+        return String.format("Wallet overall summary: %s", wallet.getWalletOverallSummary(currentPrices));
+    }
+
+}
