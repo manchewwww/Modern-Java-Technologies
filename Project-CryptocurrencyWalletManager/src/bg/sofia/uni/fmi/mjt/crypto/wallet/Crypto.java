8af9cdd37098682cd7c45cd3985b2caa14d50fb6
manchewwww <georgimanchev2003@gmail.com>
2025-01-29 14:49:05 +0200

    Add type_is_crypto parameter

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/wallet/Crypto.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/wallet/Crypto.java
@@ -3,9 +3,10 @@ package bg.sofia.uni.fmi.mjt.crypto.wallet;
 import com.google.gson.annotations.SerializedName;
 
 public record Crypto(@SerializedName(ASSET_ID) String assetId, @SerializedName(PRICE_USD) double priceUsd,
-                     double quantity) {
+                     @SerializedName(TYPE_IS_CRYPTO) int typeIsCrypto, double quantity) {
 
     private static final String ASSET_ID = "asset_id";
     private static final String PRICE_USD = "price_usd";
+    private static final String TYPE_IS_CRYPTO = "type_is_crypto";
 
 }
