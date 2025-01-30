a29ff445d2b8b3664bdc93111363c11f45745a74
manchewwww <georgimanchev2003@gmail.com>
2025-01-30 13:04:06 +0200

    Replace CacheData with DataRepository

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/commands/CommandFactory.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/commands/CommandFactory.java
@@ -3,6 +3,7 @@ package bg.sofia.uni.fmi.mjt.crypto.commands;
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.InvalidCommandException;
 import bg.sofia.uni.fmi.mjt.crypto.server.api.CoinApi;
 import bg.sofia.uni.fmi.mjt.crypto.server.api.data.CacheData;
+import bg.sofia.uni.fmi.mjt.crypto.server.repository.DataRepository;
 import bg.sofia.uni.fmi.mjt.crypto.server.repository.UserRepository;
 
 import java.nio.channels.SocketChannel;
@@ -10,11 +11,11 @@ import java.nio.channels.SocketChannel;
 public class CommandFactory {
 
     private final UserRepository userRepository;
-    private final CacheData cacheData;
+    private final DataRepository dataRepository;
 
-    public CommandFactory(UserRepository userRepository, CacheData cacheData) {
+    public CommandFactory(UserRepository userRepository, DataRepository dataRepository) {
         this.userRepository = userRepository;
-        this.cacheData = cacheData;
+        this.dataRepository = dataRepository;
     }
 
     public Command getCommand(String command, SocketChannel channel) throws InvalidCommandException {
@@ -24,11 +25,11 @@ public class CommandFactory {
             case "login" -> new LoginCommand(userRepository, channel);
             case "logout" -> new LogOutCommand(channel);
             case "deposit-money" -> new DepositMoneyCommand(userRepository, channel);
-            case "buy" -> new BuyCommand(userRepository, channel, cacheData);
-            case "sell" -> new SellCommand(userRepository, channel, cacheData);
-            case "list-offerings" -> new ListOfferingsCommand(cacheData);
+            case "buy" -> new BuyCommand(userRepository, channel, dataRepository.getCacheData());
+            case "sell" -> new SellCommand(userRepository, channel, dataRepository.getCacheData());
+            case "list-offerings" -> new ListOfferingsCommand(dataRepository.getCacheData());
             case "get-wallet-summary" -> new GetWalletSummaryCommand(userRepository, channel);
-            case "get-wallet-overall-summary" -> new GetWalletOverallSummaryCommand(userRepository, channel, cacheData);
+            case "get-wallet-overall-summary" -> new GetWalletOverallSummaryCommand(userRepository, channel, dataRepository.getCacheData());
             default -> throw new InvalidCommandException("Invalid command: " + command);
         };
     }
