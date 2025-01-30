4f0db2ccb41a88ff3e1b7182e31cd68fae7151d6
manchewwww <georgimanchev2003@gmail.com>
2025-01-30 13:16:02 +0200

    Add two messages for exception in InMemoryUserRepository

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/messages/ErrorMessages.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/messages/ErrorMessages.java
@@ -3,5 +3,7 @@ package bg.sofia.uni.fmi.mjt.crypto.messages;
 public class ErrorMessages {
 
     public static final String INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of arguments";
+    public static final String USER_EXISTS_MESSAGE = "User with username: %s already exists";
+    public static final String USER_DOES_NOT_EXISTS_MESSAGE = "User with username: %s doesn't exists";
 
 }
