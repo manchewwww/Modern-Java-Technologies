9ad55de4e8d473edc5b6a9e145614e0359341fdc
manchewwww <georgimanchev2003@gmail.com>
2025-01-30 13:32:35 +0200

    Add messages for exceptions in StatusCodeFactory

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/messages/ErrorMessages.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/messages/ErrorMessages.java
@@ -5,5 +5,17 @@ public class ErrorMessages {
     public static final String INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of arguments";
     public static final String USER_EXISTS_MESSAGE = "User with username: %s already exists";
     public static final String USER_DOES_NOT_EXISTS_MESSAGE = "User with username: %s doesn't exists";
+    public static final String CRYPTO_DOES_NOT_EXIST_MESSAGE = "Crypto with this asset id is not found";
+
+    public static final String BAD_REQUEST_MESSAGE = "Bad Request -- There is something wrong with your request";
+    public static final String UNAUTHORIZED_MESSAGE = "Unauthorized -- Your API key is wrong";
+    public static final String FORBIDDEN_MESSAGE =
+        "Forbidden -- Your API key doesn't have enough privileges to access this resource";
+    public static final String TOO_MANY_REQUESTS_MESSAGE =
+        "Too many requests -- You have exceeded your API key rate limits";
+    public static final String NO_DATA_MESSAGE =
+        "No data -- You requested specific single item that we don't have at this moment.";
+    public static final String UNEXPECTED_RESPONSE_CODE_EXCEPTION_MESSAGE =
+        "Unexpected response code from weather forecast service";
 
 }
