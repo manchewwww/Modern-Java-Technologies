2168cd0498e1e2fa057bfbe6d9165d94eba87695
manchewwww <georgimanchev2003@gmail.com>
2025-02-05 15:29:55 +0200

    Make methods and loginSocketChannel non static

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/user/UserSessionManager.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/user/UserSessionManager.java
@@ -1,37 +1,48 @@
 package bg.sofia.uni.fmi.mjt.crypto.user;
 
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.NotLoginException;
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.LoginException;
+import bg.sofia.uni.fmi.mjt.crypto.messages.ErrorMessages;
+
 import java.nio.channels.SocketChannel;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class UserSessionManager {
 
-    private static final Map<SocketChannel, String> SOCKET = new ConcurrentHashMap<>();
+    private static final String LOGIN_SUCCESSFUL = "You are log in successfully";
+    private static final String LOGOUT_SUCCESSFUL = "You are logged out successfully";
 
-    public static String addSession(SocketChannel channel, String username) {
-        if (SOCKET.containsKey(channel)) {
-            throw new IllegalArgumentException("You are logged in profile!");
-        }
+    private final Map<SocketChannel, String> loginSocketChannels;
 
-        SOCKET.put(channel, username);
-        return "You are log in successfully";
+    public UserSessionManager() {
+        loginSocketChannels = new ConcurrentHashMap<>();
     }
 
-    public static String removeSession(SocketChannel channel) {
-        if (!SOCKET.containsKey(channel)) {
-            throw new IllegalArgumentException("You are not logged in profile!");
+    public String addSession(SocketChannel channel, String username) throws LoginException {
+        if (loginSocketChannels.containsKey(channel)) {
+            throw new LoginException(ErrorMessages.YOU_ARE_LOGIN_MESSAGE);
         }
 
-        SOCKET.remove(channel);
-        return "You are logged out successfully";
+        loginSocketChannels.put(channel, username);
+        return LOGIN_SUCCESSFUL;
     }
 
-    public static String getUsername(SocketChannel channel) {
-        if (!SOCKET.containsKey(channel)) {
-            throw new IllegalArgumentException("You are not logged in profile!");
+    public String removeSession(SocketChannel channel) throws NotLoginException {
+        if (!loginSocketChannels.containsKey(channel)) {
+            throw new NotLoginException(ErrorMessages.YOU_ARE_NOT_LOGIN_MESSAGE);
         }
 
-        return SOCKET.get(channel);
+        loginSocketChannels.remove(channel);
+        return LOGOUT_SUCCESSFUL;
+    }
+
+    public String getUsername(SocketChannel channel) throws NotLoginException {
+        if (!loginSocketChannels.containsKey(channel)) {
+            throw new NotLoginException(ErrorMessages.YOU_ARE_NOT_LOGIN_MESSAGE);
+        }
+
+        return loginSocketChannels.get(channel);
     }
 
 }
