56e491ed3b253b146377760727fceda0dd692a8d
manchewwww <georgimanchev2003@gmail.com>
2025-02-05 15:13:37 +0200

    Add one more exception in throws section

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/commands/Command.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/commands/Command.java
@@ -2,15 +2,18 @@ package bg.sofia.uni.fmi.mjt.crypto.commands;
 
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.CryptoNotFoundException;
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.InsufficientFundsException;
-import bg.sofia.uni.fmi.mjt.crypto.exceptions.InvalidAmountOfDepositException;
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.InvalidAmountException;
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.InvalidCountOfArgumentsException;
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.LoginException;
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.NotLoginException;
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.UserDoesNotExistsException;
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.UserExistsException;
 
 public interface Command {
 
     String execute(String[] args)
-        throws UserExistsException, UserDoesNotExistsException, InvalidAmountOfDepositException,
-        InvalidCountOfArgumentsException, InsufficientFundsException, CryptoNotFoundException;
+        throws UserExistsException, UserDoesNotExistsException, InvalidAmountException,
+        InvalidCountOfArgumentsException, InsufficientFundsException, CryptoNotFoundException, NotLoginException,
+        LoginException;
 
 }
