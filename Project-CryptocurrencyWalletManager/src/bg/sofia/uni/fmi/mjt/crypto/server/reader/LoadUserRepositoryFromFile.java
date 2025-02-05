c553602f0ae2c07ef3dc1b8c165217a3456546aa
manchewwww <georgimanchev2003@gmail.com>
2025-02-05 15:18:50 +0200

    Implement UserRepository reader from file

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/server/reader/LoadUserRepositoryFromFile.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/server/reader/LoadUserRepositoryFromFile.java
@@ -1,5 +1,6 @@
 package bg.sofia.uni.fmi.mjt.crypto.server.reader;
 
+import bg.sofia.uni.fmi.mjt.crypto.messages.ErrorMessages;
 import bg.sofia.uni.fmi.mjt.crypto.server.repository.InMemoryUserRepository;
 import bg.sofia.uni.fmi.mjt.crypto.server.repository.UserRepository;
 import com.google.gson.Gson;
@@ -23,7 +24,7 @@ public class LoadUserRepositoryFromFile {
         try (FileReader fileWriter = new FileReader(PATH.toFile())) {
             return GSON.fromJson(fileWriter, InMemoryUserRepository.class);
         } catch (IOException e) {
-            throw new RuntimeException("Could not load user repository from file");
+            throw new RuntimeException(ErrorMessages.LOAD_USERS_ERROR);
         }
     }
 
