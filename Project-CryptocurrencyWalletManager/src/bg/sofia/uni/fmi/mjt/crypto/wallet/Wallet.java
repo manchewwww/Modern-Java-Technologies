020da24437488e3dd7874a386ae54620d8ad35ac
manchewwww <georgimanchev2003@gmail.com>
2025-02-05 15:31:03 +0200

    Extract condition checks into separate functions for readability

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/wallet/Wallet.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/wallet/Wallet.java
@@ -2,7 +2,8 @@ package bg.sofia.uni.fmi.mjt.crypto.wallet;
 
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.CryptoNotFoundException;
 import bg.sofia.uni.fmi.mjt.crypto.exceptions.InsufficientFundsException;
-import bg.sofia.uni.fmi.mjt.crypto.exceptions.InvalidAmountOfDepositException;
+import bg.sofia.uni.fmi.mjt.crypto.exceptions.InvalidAmountException;
+import bg.sofia.uni.fmi.mjt.crypto.messages.ErrorMessages;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -11,14 +12,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Wallet {
-
-    private static final String INVALID_DEPOSIT_AMOUNT_MESSAGE = "Deposit amount must be greater than 0.0";
-    private static final String INVALID_CURRENT_PRICE_MESSAGE = "Current price must be greater than zero.";
-    private static final String CRYPTO_ASSETS_ID_NULL_MESSAGE = "Crypto asset id cannot be null.";
-    private static final String INSUFFICIENT_BALANCE_MESSAGE = "You don't have enough money.";
-    private static final String CRYPTO_DOES_NOT_EXIST_MESSAGE = "Crypto asset id does not exist.";
-    private static final String CURRENT_PRICES_NULL_MESSAGE = "currentPrices cannot be null";
-    private static final String NO_INVESTMENTS_FOUND_MESSAGE = "No investments found.";
 
     private static final double MINIMUM_AMOUNT = 0.0;
     private static final int TYPE_CRYPTO = 1;
@@ -40,31 +33,23 @@ public class Wallet {
         return cryptoInvestments;
     }
 
-    public void depositMoney(double amount) throws InvalidAmountOfDepositException {
-        if (amount <= MINIMUM_AMOUNT) {
-            throw new InvalidAmountOfDepositException(INVALID_DEPOSIT_AMOUNT_MESSAGE);
-        }
+    public void depositMoney(double amount) throws InvalidAmountException {
+        checkAmountOfMoney(amount);
 
         synchronized (this) {
             this.balance += amount;
         }
     }
 
-    public void buyCrypto(String assetId, double currentPrice, double amount)
-        throws InvalidAmountOfDepositException, InsufficientFundsException {
-        if (assetId == null) {
-            throw new IllegalArgumentException(CRYPTO_ASSETS_ID_NULL_MESSAGE);
-        }
-        if (currentPrice <= MINIMUM_AMOUNT) {
-            throw new IllegalArgumentException(INVALID_CURRENT_PRICE_MESSAGE);
-        }
-        if (amount <= MINIMUM_AMOUNT) {
-            throw new InvalidAmountOfDepositException(INVALID_DEPOSIT_AMOUNT_MESSAGE);
-        }
+    public void buyCrypto(String assetId, double amount,  double currentPrice)
+        throws InvalidAmountException, InsufficientFundsException, CryptoNotFoundException {
+        checkAssetID(assetId);
+        checkCurrentPrice(currentPrice);
+        checkAmountOfMoney(amount);
 
         synchronized (this) {
             if (amount > this.balance) {
-                throw new InsufficientFundsException(INSUFFICIENT_BALANCE_MESSAGE);
+                throw new InsufficientFundsException(ErrorMessages.INSUFFICIENT_BALANCE_MESSAGE);
             }
             this.balance -= amount;
 
@@ -74,17 +59,13 @@ public class Wallet {
         }
     }
 
-    public void sell(String assetId, double currentPrice) throws CryptoNotFoundException {
-        if (assetId == null) {
-            throw new IllegalArgumentException(CRYPTO_ASSETS_ID_NULL_MESSAGE);
-        }
-        if (currentPrice <= MINIMUM_AMOUNT) {
-            throw new IllegalArgumentException(INVALID_CURRENT_PRICE_MESSAGE);
-        }
+    public void sell(String assetId, double currentPrice) throws CryptoNotFoundException, InvalidAmountException {
+        checkAssetID(assetId);
+        checkCurrentPrice(currentPrice);
 
         synchronized (this) {
             if (!cryptoInvestments.containsKey(assetId)) {
-                throw new CryptoNotFoundException(CRYPTO_DOES_NOT_EXIST_MESSAGE);
+                throw new CryptoNotFoundException(ErrorMessages.CRYPTO_DOES_NOT_EXIST_MESSAGE);
             }
 
             double allCryptoWithAssetId = cryptoInvestments.get(assetId).stream()
@@ -104,13 +85,13 @@ public class Wallet {
     public synchronized String getWalletOverallSummary(Map<String, Double> currentPrices)
         throws CryptoNotFoundException {
         if (currentPrices == null) {
-            throw new IllegalArgumentException(CURRENT_PRICES_NULL_MESSAGE);
+            throw new CryptoNotFoundException(ErrorMessages.CURRENT_PRICES_NULL_MESSAGE);
         }
         if (cryptoInvestments.isEmpty()) {
-            return NO_INVESTMENTS_FOUND_MESSAGE;
+            return ErrorMessages.NO_INVESTMENTS_FOUND_MESSAGE;
         }
         if (!currentPrices.keySet().containsAll(cryptoInvestments.keySet())) {
-            throw new CryptoNotFoundException(CRYPTO_DOES_NOT_EXIST_MESSAGE);
+            throw new CryptoNotFoundException(ErrorMessages.CRYPTO_DOES_NOT_EXIST_MESSAGE);
         }
 
         double investedAmountInCrypto = cryptoInvestments.values().stream()
@@ -131,6 +112,24 @@ public class Wallet {
             currentAmountInCrypto - investedAmountInCrypto);
 
         return GSON.toJson(walletSummary);
+    }
+
+    private void checkAmountOfMoney(double amount) throws InvalidAmountException {
+        if (amount <= MINIMUM_AMOUNT) {
+            throw new InvalidAmountException(ErrorMessages.INVALID_DEPOSIT_AMOUNT_MESSAGE);
+        }
+    }
+
+    private void checkAssetID(String assetId) throws CryptoNotFoundException {
+        if (assetId == null) {
+            throw new CryptoNotFoundException(ErrorMessages.CRYPTO_ASSETS_ID_NULL_MESSAGE);
+        }
+    }
+
+    private void checkCurrentPrice(double currentPrice) throws InvalidAmountException {
+        if (currentPrice <= MINIMUM_AMOUNT) {
+            throw new InvalidAmountException(ErrorMessages.INVALID_CURRENT_PRICE_MESSAGE);
+        }
     }
 
 }
