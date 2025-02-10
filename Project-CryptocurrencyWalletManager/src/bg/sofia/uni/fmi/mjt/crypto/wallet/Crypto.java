faa0a86888f4091b3a4d0d876cea14f07a3ee501
manchewwww <georgimanchev2003@gmail.com>
2025-02-10 13:13:43 +0200

    Add static method of

--- Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/wallet/Crypto.java
+++ Project-CryptocurrencyWalletManager/src/bg/sofia/uni/fmi/mjt/crypto/wallet/Crypto.java
@@ -2,11 +2,17 @@ package bg.sofia.uni.fmi.mjt.crypto.wallet;
 
 import com.google.gson.annotations.SerializedName;
 
-public record Crypto(@SerializedName(ASSET_ID) String assetId, @SerializedName(PRICE_USD) double priceUsd,
-                     @SerializedName(TYPE_IS_CRYPTO) int typeIsCrypto, double quantity) {
+public record Crypto(@SerializedName(ASSET_ID) String assetId,
+                     @SerializedName(PRICE_USD) double priceUsd,
+                     @SerializedName(TYPE_IS_CRYPTO) int typeIsCrypto,
+                     double quantity) {
 
     private static final String ASSET_ID = "asset_id";
     private static final String PRICE_USD = "price_usd";
     private static final String TYPE_IS_CRYPTO = "type_is_crypto";
+
+    public static Crypto of(String assetId, double priceUsd, int typeIsCrypto, double quantity) {
+        return new Crypto(assetId, priceUsd, typeIsCrypto, quantity);
+    }
 
 }
